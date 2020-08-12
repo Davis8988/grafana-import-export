662595f2b862ceb321dbf55aa50b8222128e3041
Davis8988 <Davismarsel@gmail.com>
2020-08-12 23:06:20 +0300

    Powershell/add scripts (#1)
    * Added main exporter script
    
    * Added powershell dir to gitignore & Added grafanaApi() function
    
    * Added color printings
    
    * Updated export
    
    * Added import & delete scripts
    
    * Corrected export & import of folders
    
    * Finished with powershell scripts

---
+++ powershell/ExportGrafanaDashboards.ps1
@@ -0,0 +1,189 @@
+﻿# 
+# This script exports Grafana datasources, folders & dashboards.
+# 
+# Psrerequisites:
+#   Powershell v2 or higher
+#   Grafana API Key: Settings -> API Keys (A viewer-key will suffice only for dashboards and folders. An admin-key will be required for datasources)
+# 
+# If using powershell v2: $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$scriptPath = $PSScriptRoot
+
+# Save export data to:
+$dashboardsDir = "$scriptPath\exported_dashboards"
+$datasourcesDir = "$scriptPath\exported_datasources"
+$foldersDir = "$scriptPath\exported_folders"
+
+
+$grafana_home_url = "http://localhost:3000"
+$api_key = "eyJrIjoicWhLR09QNDVkbnZzWDRGUURCTlRMM1ZvNlJNVnR2SzAiLCJuIjoid29ybGQiLCJpZCI6MX0="
+
+
+# No need to touch this
+$convertJsonDepth = 100
+
+function grafanaApiCall() {
+	Param(
+		[parameter(Mandatory=$true, Position=0)]
+		[String] $grafanaHost,
+		[parameter(Mandatory=$true, Position=1)]
+		[String] $apiType,
+		[parameter(Mandatory=$true, Position=2)]
+		[String] $apiKey
+    )
+	# Do not change unless you know what you are doing:
+	$grafana_api_error_msg = "If you're seeing this Grafana has failed to load its application files"
+	$basicAuth = [string]::Format("Bearer {0}", "${apiKey}")
+	$headers = @{"Authorization" = $basicAuth }
+	
+	$grafanaFullUri = "$grafanaHost/api/$apiType"
+	try {
+		$resultData = Invoke-RestMethod -Uri $grafanaFullUri -Method GET -Headers $headers -ContentType 'application/json' -ErrorAction stop
+		
+		# Check for errors:
+		if (($resultData -eq $null) -or ($resultData -Like "*${grafana_api_error_msg}*")) {Read-Host "Failed querying grafana api at: $grafanaFullUri using defined 'api_key' in this script"; exit 1}
+		return $resultData
+	} catch {
+		# Note that value__ is not a typo.
+		$errorCode = $_.Exception.Response.StatusCode.value__ 
+		Write-Host "StatusCode:" $errorCode
+		Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
+		Write-Host "Exception Message:" $_.Exception.Message
+		if ($errorCode -eq 403) {Write-Warning "This might indicate that your API key is for viewer/editor only and not an Admin one"}
+		Read-Host "Press Enter to continue.."
+		exit 1
+	}
+}
+
+
+function saveFolderJsonFile() {
+	Param(
+		[parameter(Mandatory=$true, Position=0)]
+		[Object []] $dash_Content
+    )
+	
+	$folderJsonFormat_Content =  @{
+		"title"= $dash_Content.dashboard.title
+		"uid" = $dash_Content.dashboard.uid
+		"id" = $dash_Content.dashboard.id
+		"overwrite" = $true
+	}
+	$folder_title = $folderJsonFormat_Content.title
+	Write-Host -NoNewLine "Exporting folder: "
+	Write-Host "$folder_title" -ForegroundColor Yellow
+	
+	$folderJsonFormat_Content | ConvertTo-Json | Out-File "$foldersDir\$folder_title.json"
+}
+
+function saveDashboardJsonFile() {
+	Param(
+		[parameter(Mandatory=$true, Position=0)]
+		[Object []] $dash_Content
+    )
+	
+	
+	$dash_title = $dash_Content.dashboard.title
+	$folder_id = $dash_Content.meta.folderId
+	$dash_Content.dashboard.id = $null   # When importing always assign $null to the dashboard.id field
+	$dash_Content.dashboard.uid = $null  # When importing if setting dashboard.uid field to $null it will create a new dashboard. But will overwrite an existing dashboard otherwise
+	# $dash_Content.dashboard.version = 1  # Reset dashboard version
+	# $dash_Content.meta.version = 1
+	$dash_Content | Add-Member -Type NoteProperty -Name 'overwrite' -Value $true -Force  # Add {"overwrite" : true} property
+	$dash_Content | Add-Member -Type NoteProperty -Name 'folderId' -Value $folder_id -Force  # Add {"folderId" : $id} property
+	Write-Host -NoNewLine "Exporting dashboard: "
+	Write-Host "$dash_title " -ForegroundColor Yellow
+	
+	$dash_Content | ConvertTo-Json -Depth $convertJsonDepth | Out-File "$dashboardsDir\$dash_title.json" -Force
+}
+
+
+function exportDashboardsAndFolders() {
+	$grafana_dashboards_api_url = "${grafana_home_url}/api/search"
+	Write-Host "Exporting dashborads from: $grafana_home_url"
+	Write-Host "Querying for dashboards and folders"
+	$dashboards = grafanaApiCall "$grafana_home_url" "search" "$api_key"
+	
+	Write-Host "Found $($dashboards.count) dashboards" -ForegroundColor Cyan
+
+	Write-Host "Exporting dashboards & folders to: `r`n - ${dashboardsDir} `r`n - ${foldersDir}"
+	New-Item -ItemType Directory -Path "$dashboardsDir" -Force | Out-Null
+	New-Item -ItemType Directory -Path "$foldersDir" -Force | Out-Null
+	Write-Host ""
+
+	foreach ($dash in $dashboards) {
+		try {
+			$dash_url = "${grafana_home_url}/api/dashboards/uid/$($dash.uid)"
+			$dash_Content = grafanaApiCall "$grafana_home_url" "dashboards/uid/$($dash.uid)" "$api_key"
+			
+			if ($dash_Content.meta.isFolder) {
+				saveFolderJsonFile $dash_Content
+			} else {
+				saveDashboardJsonFile $dash_Content
+			}
+			
+			Write-Host "Success" -ForegroundColor Green
+			
+		} catch {
+			Write-Host $_
+			Write-Host "Failed to export $dash" -ForegroundColor Red
+		}
+	}
+	Write-Host ""
+	
+}
+
+function exportDatasources() {
+	Write-Host "Exporting datasources from: $grafana_home_url"
+	Write-Host "Querying for datasources"
+	$datasources = grafanaApiCall "$grafana_home_url" "datasources" "$api_key"
+
+	Write-Host "Found $($datasources.count) datasources" -ForegroundColor Cyan
+
+	Write-Host "Exporting datasources to: `r`n - ${datasourcesDir}"
+	New-Item -ItemType Directory -Path "$datasourcesDir" -Force | Out-Null
+	Write-Host ""
+	
+	foreach ($dataSrc in $datasources) {
+		try {
+			$dataSrc_url = "${grafana_home_url}/api/datasources/$($dataSrc.id)"
+			
+			$dataSrc_Content = grafanaApiCall "$grafana_home_url" "datasources/$($dataSrc.id)" "$api_key"
+
+			$dataSrc_Name = $dataSrc_Content.name
+			$dataSrc_Content.id = $null   # When importing always assign $null to the datasource.id field
+			
+			Write-Host -NoNewLine "Exporting datasource: "
+			Write-Host "$dataSrc_Name " -ForegroundColor Yellow
+			$dataSrc_Content | ConvertTo-Json | Out-File "$datasourcesDir\$dataSrc_Name.json"
+			Write-Host "Success" -ForegroundColor Green
+		} catch {
+			Write-Host $_
+			Write-Host "Failed to export $dash" -ForegroundColor Red
+		}
+	}
+	Write-Host ""
+	
+}
+
+
+
+
+function main() {
+	Write-Host "Exporter Started"
+	exportDashboardsAndFolders
+	exportDatasources
+	Write-Host "Exporter Finished"
+	Read-Host
+}
+
+
+main
+
+
+
+
+
+
+
+
+
+
---
+++ powershell/ImportGrafanaDashboards.ps1
@@ -0,0 +1,146 @@
+﻿# 
+# This script imports Grafana datasources, folders & dashboards.
+#  it will overwrite existing datasources & dashboards
+# 
+# Psrerequisites:
+#   Powershell v2 or higher
+#   Grafana API Key: Settings -> API Keys (A viewer-key will suffice only for dashboards and folders. An admin-key will be required for datasources)
+# 
+# If using powershell v2: $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$scriptPath = $PSScriptRoot
+
+$dashboardsDir = "$scriptPath\exported_dashboards"
+$foldersDir = "$scriptPath\exported_folders"
+$datasourcesDir = "$scriptPath\exported_datasources"
+
+$grafana_home_url = "http://localhost:3000"
+$api_key = "eyJrIjoicWhLR09QNDVkbnZzWDRGUURCTlRMM1ZvNlJNVnR2SzAiLCJuIjoid29ybGQiLCJpZCI6MX0="
+
+# Importing a folder that already exists gives this error
+$folderExistErrorMsg = "A folder or dashboard in the general folder with the same name already exists"
+
+# Show error for X seconds
+$failurePause_SleepSec = 2
+
+# Shows debug messages only
+# $DebugPreference = 'Continue'
+
+
+function grafanaApiCall() {
+	Param(
+		[parameter(Mandatory=$true, Position=0)]
+		[String] $actionDescription,
+		[parameter(Mandatory=$true, Position=1)]
+		[String] $apiPath,
+		[parameter(Mandatory=$true, Position=3)]
+		[String] $methodType,
+		[parameter(Position=4)]
+		[String] $body = $null
+    )
+	
+	# Do not change unless you know what you are doing:
+	$grafana_api_error_msg = "If you're seeing this Grafana has failed to load its application files"
+	$basicAuth = [string]::Format("Bearer {0}", "${api_key}")
+	$headers = @{"Authorization" = $basicAuth }
+	
+	$grafanaFullUri = "$grafana_home_url/api/$apiPath"
+	try {
+		# Prints
+		if ($body) {Write-Debug "Executing: Invoke-RestMethod -Uri $grafanaFullUri -Method $methodType -Headers $headers -ContentType 'application/json' -Body $body -ErrorAction Stop"} 
+		else       {Write-Debug "Executing: Invoke-RestMethod -Uri $grafanaFullUri -Method $methodType -Headers $headers -ContentType 'application/json' -ErrorAction Stop"} 
+		
+		# Execute
+		if ($body) {$resultData = Invoke-RestMethod -Uri $grafanaFullUri -Method $methodType -Headers $headers -ContentType 'application/json' -Body $body -ErrorAction Stop} 
+		else       {$resultData = Invoke-RestMethod -Uri $grafanaFullUri -Method $methodType -Headers $headers -ContentType 'application/json' -ErrorAction Stop} 
+		
+		# Check for errors:
+		if ((-not $?) -or ($resultData -eq $null) -or ($resultData -Like "*${grafana_api_error_msg}*")) {Read-Host "Error - Failed $actionDescription"; Start-Sleep $failurePause_SleepSec; exit 1}
+		
+		return $resultData
+	} catch {
+		# Note that value__ is not a typo.
+		if ($_ -Like "*$folderExistErrorMsg*") {Write-Host "Already exists" -ForegroundColor Green; continue}
+		$errorCode = $_.Exception.Response.StatusCode.value__ 
+		Write-Host "Server Message: $_"
+		Write-Host "StatusCode:" $errorCode
+		Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
+		Write-Host "Exception Message:" $_.Exception.Message
+		if ($errorCode -eq 403) {Write-Warning "This might indicate that your API key is for viewer/editor only and not an Admin one"}
+		elseif ($errorCode -eq 409) {Write-Warning "OK - This probably indicates that the imported datasource already exists"}
+		elseif ($errorCode -eq 412) {Write-Warning "OK - This probably indicates that the imported folder already exists"}
+		Start-Sleep $failurePause_SleepSec
+		return $false
+	}
+	
+}
+
+
+function importJsonFiles() {
+	Param(
+		[parameter(Mandatory=$true, Position=0)]
+		[String] $importTitle,
+		[parameter(Mandatory=$true, Position=1)]
+		[String] $jsonFilesDir,
+		[parameter(Mandatory=$true, Position=2)]
+		[String] $apiPath
+    )
+	Write-Host "Importing all ${importTitle} json files from: $jsonFilesDir"
+	
+	$jsonFiles = Get-ChildItem -Path $jsonFilesDir -Recurse -Include *.json
+	Write-Host "Found $($jsonFiles.count) $importTitle jsons files" -ForegroundColor Cyan
+	
+	foreach ($jsonFile in $jsonFiles) {
+		try {
+			$jsonFile_rawContent = Get-Content -Path $jsonFile -Raw -ErrorAction Stop
+			$jsonFile_JsonContent = $jsonFile_rawContent | ConvertFrom-Json
+			$itemName = $jsonFile_JsonContent.name
+			$folderUid = $jsonFile_JsonContent.uid
+			
+			Write-Host -NoNewLine "Importing ${importTitle}: "
+			Write-Host "$($jsonFile.BaseName) " -ForegroundColor Yellow
+			$actionDesc = "importing $importTitle $($jsonFile.BaseName)"
+			
+			if ($importTitle -eq "Datasource") {
+				grafanaApiCall $actionDesc "${apiPath}/name/${itemName}" "DELETE" $jsonFile_rawContent
+				$apiCallResult = grafanaApiCall $actionDesc "${apiPath}" "POST" $jsonFile_rawContent | Select -Last 1  # Update an existing datasource
+			}
+			elseif ($importTitle -eq "Folder") {
+				grafanaApiCall $actionDesc "${apiPath}" "POST" $jsonFile_rawContent | Out-Null # Try to create the folder - might receive an error it already exists but thats ok..
+				$apiCallResult = grafanaApiCall $actionDesc "${apiPath}/${folderUid}" "PUT" $jsonFile_rawContent | Select -Last 1  # Update an existing folder
+			}
+			else {
+				$apiCallResult = grafanaApiCall $actionDesc "${apiPath}" "POST" $jsonFile_rawContent | Select -Last 1  # Update an existing dashboard
+			}
+			
+			if ($apiCallResult) {Write-Host "Success" -ForegroundColor Green} else {Write-Host "Failure" -ForegroundColor Red}
+			Write-Host ""
+		} catch {
+			Write-Host $_
+			Write-Host "Failed to import ${importTitle}: $($jsonFile.BaseName)" -ForegroundColor Red
+		}
+	}
+	Write-Host ""
+}
+
+
+function main() {
+	Write-Host "Importer Started"
+	importJsonFiles "Datasource" $datasourcesDir "datasources"
+	importJsonFiles "Folder"     $foldersDir     "folders"
+	importJsonFiles "Dashboard"  $dashboardsDir  "dashboards/db"
+	Write-Host "Importer Finished"
+	Read-Host
+}
+
+
+main
+
+
+
+
+
+
+
+
+
+
